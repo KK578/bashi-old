9dc2afe6a1b79e1bb040467826c9b060f59c5bde
Kevin Kwan <KevinKwan95@googlemail.com>
2018-01-29 20:01:05 +0000

    BASHI-63: Fix Test and add another for ReceiveData

--- test/SlackApi.Rtm.Test/Client/WebSocketManagerTest.cs
+++ test/SlackApi.Rtm.Test/Client/WebSocketManagerTest.cs
@@ -20,13 +20,45 @@ namespace SlackApi.Rtm.Test.Client
             {
                 mock.Mock<IClientWebSocket>()
                     .Setup(x => x.ConnectAsync(It.IsAny<Uri>(), It.IsAny<CancellationToken>()))
-                    .Returns(new Task(() => { }));
+                    .Returns(Task.Delay(0));
                 subject = mock.Create<WebSocketManager>();
 
                 await subject.ConnectAsync(new Uri("http://localhost"));
 
                 mock.Mock<IClientWebSocket>()
-                    .Verify(x => x.ConnectAsync(It.IsAny<Uri>(), It.IsAny<CancellationToken>()));
+                    .Verify(x => x.ConnectAsync(It.IsAny<Uri>(), It.IsAny<CancellationToken>()), Times.Once);
+            }
+        }
+
+        [Test]
+        public async Task ReceiveData_ReturnsResultFromBuffer()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IClientWebSocket>()
+                    .Setup(x => x.ReceiveAsync(It.IsAny<ArraySegment<byte>>(), It.IsAny<CancellationToken>()))
+                    .Callback<ArraySegment<byte>, CancellationToken>((buffer, token) =>
+                                                                     {
+                                                                         buffer.Array[0] = (byte)'H';
+                                                                         buffer.Array[1] = (byte)'e';
+                                                                         buffer.Array[2] = (byte)'l';
+                                                                         buffer.Array[3] = (byte)'l';
+                                                                         buffer.Array[4] = (byte)'o';
+
+                                                                         for (var i = 5; i < 1024; i++)
+                                                                         {
+                                                                             buffer.Array[i] = (byte)' ';
+                                                                         }
+                                                                     });
+                subject = mock.Create<WebSocketManager>();
+
+                var result = await subject.ReceiveData();
+
+                Assert.That(result, Is.EqualTo("Hello"));
+
+                mock.Mock<IClientWebSocket>()
+                    .Verify(x => x.ReceiveAsync(It.IsAny<ArraySegment<byte>>(), It.IsAny<CancellationToken>()),
+                            Times.Once);
             }
         }
     }
