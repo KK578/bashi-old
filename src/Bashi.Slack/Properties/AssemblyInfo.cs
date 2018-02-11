e9c0956f0b3b95b6556b70ec616f2cfe8d521788
Kevin Kwan <KevinKwan95@googlemail.com>
2018-02-11 16:25:28 +0000

    BASHI-68: Add test for SlackConnectionManager

--- src/Bashi.Slack/Properties/AssemblyInfo.cs
+++ src/Bashi.Slack/Properties/AssemblyInfo.cs
@@ -1,7 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bashi.Slack")]
@@ -13,10 +14,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("Bashi.Slack.Test")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("A1DFF872-8384-4E1A-A8A5-5A2FE00886AC")]
@@ -24,11 +26,11 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ test/Bashi.Slack.Test/Connection/SlackConnectionManagerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using Bashi.Slack.Connection;
+using Moq;
+using NUnit.Framework;
+using SlackApi.Core.Data.Message.Web.Response.Rtm;
+using SlackApi.Core.Interface.Rtm;
+using SlackApi.Core.Interface.Web;
+
+namespace Bashi.Slack.Test.Connection
+{
+    public class SlackConnectionManagerTest
+    {
+        private SlackConnectionManager subject;
+
+        private void SetUp(string url)
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var response = new RtmConnectResponse { WebSocketUrl = url };
+
+                mock.Mock<ISlackWebClient>()
+                    .Setup(x => x.RtmConnectAsync(It.IsAny<string>()))
+                    .Returns(Task.FromResult(response));
+                mock.Mock<ISlackRtmClient>()
+                    .Setup(x => x.ConnectAsync(It.IsAny<string>()))
+                    .Returns(Task.Delay(0));
+
+                subject = mock.Create<SlackConnectionManager>();
+            }
+        }
+
+        [Test]
+        public void Connect_GivenEmptyStringShouldFail()
+        {
+            SetUp("");
+            Assert.Throws<Exception>(() => subject.Connect());
+        }
+
+        [Test]
+        public void Connect_ShouldAttemptAConnection()
+        {
+            SetUp("http://localhost");
+            subject.Connect();
+        }
+    }
+}
